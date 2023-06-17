a862dd85667089bd70f7af616b365d1dce5790f5
Atakan <atakangobelek@satta.biz>
2023-06-17 17:54:45 +0300

    proposal added

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 
 import '../view/login_view/login_view.dart';
 import '../view/order_view/order_view.dart';
+import '../view/proposal_view/proposal_view.dart';
 
 
 
 final Map<String, WidgetBuilder> routes = {
   '/login': (BuildContext context) => LoginView(),
   '/orderScreen': (BuildContext context) => OrderView(),
-  /* '/proposalScreen': (BuildContext context) => proposalView(), */ //proposal viewda galdin
+  '/proposalScreen': (BuildContext context) => proposalView(),
 
 
 };
