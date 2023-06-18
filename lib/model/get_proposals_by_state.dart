fbd5e06123bd9ec2588f2638aa5937c35cac0038
Atakan <atakangobelek@satta.biz>
2023-06-18 19:16:06 +0300

    refactor small card

--- lib/model/get_proposals_by_state.dart
+++ lib/model/get_proposals_by_state.dart
@@ -1,8 +1,6 @@
-// To parse this JSON data, do
-//
-//     final getProposalModel = getProposalModelFromMap(jsonString);
-
 import 'dart:convert';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 
 class GetProposalModel {
   int? proposalId;
@@ -19,8 +17,8 @@ class GetProposalModel {
   String? supplierCompany;
   String? supplierErpId;
   int? supplierCompanyId;
-  dynamic supervisor;
-  dynamic paymentType;
+  String? supervisor;
+  String? paymentType;
   List<ProductProposal>? productProposals;
 
   GetProposalModel({
@@ -42,9 +40,10 @@ class GetProposalModel {
     this.paymentType,
     this.productProposals,
   });
+
   @override
   String toString() {
-    return 'GetProposalModel(proposalId: $proposalId, demandlistId: $demandlistId, proposalState: $proposalState, supplierCompany: $supplierCompany, productProposals: $productProposals)';
+    return 'GetProposalModel(proposalId: $proposalId, demandlistId: $demandlistId, proposalState: $proposalState, supplierCompany: $supplierCompany ,supplierCompany: $supplierCompany, productProposals: $productProposals)';
   }
 
   factory GetProposalModel.fromJson(String str) =>
@@ -52,59 +51,62 @@ class GetProposalModel {
 
   String toJson() => json.encode(toMap());
 
-  factory GetProposalModel.fromMap(Map<String, dynamic> json) =>
-      GetProposalModel(
-        proposalId: json["proposal_id"],
-        demandlistId: json["demandlist_id"],
-        proposalCreatedAt: json["proposal_created_at"] == null
-            ? null
-            : DateTime.parse(json["proposal_created_at"]),
-        proposalUpdatedAt: json["proposal_updated_at"] == null
-            ? null
-            : DateTime.parse(json["proposal_updated_at"]),
-        proposalState: json["proposal_state"],
-        proposalValidPeriod: json["proposal_valid_period"],
-        proposalDeliveryTime: json["proposal_delivery_time"],
-        proposalValidDate: json["proposal_valid_date"] == null
-            ? null
-            : DateTime.parse(json["proposal_valid_date"]),
-        includeShipmentCost: json["include_shipment_cost"],
-        paymentDueDate: json["payment_due_date"],
-        deliveryDate: json["delivery_date"] == null
-            ? null
-            : DateTime.parse(json["delivery_date"]),
-        supplierCompany: json["supplier_company"],
-        supplierErpId: json["supplier_erp_id"],
-        supplierCompanyId: json["supplier_company_id"],
-        supervisor: json["supervisor"],
-        paymentType: json["payment_type"],
-        productProposals: json["product_proposals"] == null
-            ? []
-            : List<ProductProposal>.from(json["product_proposals"]!
-                .map((x) => ProductProposal.fromMap(x))),
-      );
+  factory GetProposalModel.fromMap(Map<String, dynamic> json) {
+    return GetProposalModel(
+      proposalId: json["proposal_id"],
+      demandlistId: json["demandlist_id"],
+      proposalCreatedAt: json["proposal_created_at"] == null
+          ? null
+          : DateTime.parse(json["proposal_created_at"]),
+      proposalUpdatedAt: json["proposal_updated_at"] == null
+          ? null
+          : DateTime.parse(json["proposal_updated_at"]),
+      proposalState: json["proposal_state"],
+      proposalValidPeriod: json["proposal_valid_period"],
+      proposalDeliveryTime: json["proposal_delivery_time"],
+      proposalValidDate: json["proposal_valid_date"] == null
+          ? null
+          : DateTime.parse(json["proposal_valid_date"]),
+      includeShipmentCost: json["include_shipment_cost"],
+      paymentDueDate: json["payment_due_date"],
+      deliveryDate: json["delivery_date"] == null
+          ? null
+          : DateTime.parse(json["delivery_date"]),
+      supplierCompany: json["supplier_company"],
+      supplierErpId: json["supplier_erp_id"],
+      supplierCompanyId: json["supplier_company_id"],
+      supervisor: json["supervisor"],
+      paymentType: json["payment_type"],
+      productProposals: json["product_proposals"] == null
+          ? null
+          : List<ProductProposal>.from(json["product_proposals"]
+              .map((x) => ProductProposal.fromMap(x))),
+    );
+  }
 
-  Map<String, dynamic> toMap() => {
-        "proposal_id": proposalId,
-        "demandlist_id": demandlistId,
-        "proposal_created_at": proposalCreatedAt?.toIso8601String(),
-        "proposal_updated_at": proposalUpdatedAt?.toIso8601String(),
-        "proposal_state": proposalState,
-        "proposal_valid_period": proposalValidPeriod,
-        "proposal_delivery_time": proposalDeliveryTime,
-        "proposal_valid_date": proposalValidDate?.toIso8601String(),
-        "include_shipment_cost": includeShipmentCost,
-        "payment_due_date": paymentDueDate,
-        "delivery_date": deliveryDate?.toIso8601String(),
-        "supplier_company": supplierCompany,
-        "supplier_erp_id": supplierErpId,
-        "supplier_company_id": supplierCompanyId,
-        "supervisor": supervisor,
-        "payment_type": paymentType,
-        "product_proposals": productProposals == null
-            ? []
-            : List<dynamic>.from(productProposals!.map((x) => x.toMap())),
-      };
+  Map<String, dynamic> toMap() {
+    return {
+      "proposal_id": proposalId,
+      "demandlist_id": demandlistId,
+      "proposal_created_at": proposalCreatedAt?.toIso8601String(),
+      "proposal_updated_at": proposalUpdatedAt?.toIso8601String(),
+      "proposal_state": proposalState,
+      "proposal_valid_period": proposalValidPeriod,
+      "proposal_delivery_time": proposalDeliveryTime,
+      "proposal_valid_date": proposalValidDate?.toIso8601String(),
+      "include_shipment_cost": includeShipmentCost,
+      "payment_due_date": paymentDueDate,
+      "delivery_date": deliveryDate?.toIso8601String(),
+      "supplier_company": supplierCompany,
+      "supplier_erp_id": supplierErpId,
+      "supplier_company_id": supplierCompanyId,
+      "supervisor": supervisor,
+      "payment_type": paymentType,
+      "product_proposals": productProposals == null
+          ? []
+          : List<dynamic>.from(productProposals!.map((x) => x.toMap())),
+    };
+  }
 }
 
 class ProductProposal {
@@ -112,19 +114,19 @@ class ProductProposal {
   String? productName;
   String? productCategory;
   String? productUnit;
-  dynamic productErpId;
-  dynamic erpId;
+  String? productErpId;
+  String? erpId;
   int? companyProductId;
   DateTime? updatedAt;
   int? price;
-  dynamic url;
-  dynamic description;
+  String? url;
+  String? description;
   int? productId;
   String? status;
   int? updateCount;
-  dynamic equivalentId;
+  String? equivalentId;
   String? proposalNote;
-  dynamic taxRate;
+  int? taxRate;
   String? currencyCode;
   int? amount;
 
@@ -149,6 +151,7 @@ class ProductProposal {
     this.currencyCode,
     this.amount,
   });
+
   @override
   String toString() {
     return 'ProductProposal(productProposalId: $productProposalId, productName: $productName, price: $price)';
@@ -159,49 +162,53 @@ class ProductProposal {
 
   String toJson() => json.encode(toMap());
 
-  factory ProductProposal.fromMap(Map<String, dynamic> json) => ProductProposal(
-        productProposalId: json["product_proposal_id"],
-        productName: json["product_name"],
-        productCategory: json["product_category"],
-        productUnit: json["product_unit"],
-        productErpId: json["product_erp_id"],
-        erpId: json["erp_id"],
-        companyProductId: json["company_product_id"],
-        updatedAt: json["updated_at"] == null
-            ? null
-            : DateTime.parse(json["updated_at"]),
-        price: json["price"],
-        url: json["url"],
-        description: json["description"],
-        productId: json["product_id"],
-        status: json["status"],
-        updateCount: json["update_count"],
-        equivalentId: json["equivalent_id"],
-        proposalNote: json["proposal_note"],
-        taxRate: json["tax_rate"],
-        currencyCode: json["currency_code"],
-        amount: json["amount"],
-      );
+  factory ProductProposal.fromMap(Map<String, dynamic> json) {
+    return ProductProposal(
+      productProposalId: json["product_proposal_id"],
+      productName: json["product_name"],
+      productCategory: json["product_category"],
+      productUnit: json["product_unit"],
+      productErpId: json["product_erp_id"],
+      erpId: json["erp_id"],
+      companyProductId: json["company_product_id"],
+      updatedAt: json["updated_at"] == null
+          ? null
+          : DateTime.parse(json["updated_at"]),
+      price: json["price"],
+      url: json["url"],
+      description: json["description"],
+      productId: json["product_id"],
+      status: json["status"],
+      updateCount: json["update_count"],
+      equivalentId: json["equivalent_id"],
+      proposalNote: json["proposal_note"],
+      taxRate: json["tax_rate"],
+      currencyCode: json["currency_code"],
+      amount: json["amount"],
+    );
+  }
 
-  Map<String, dynamic> toMap() => {
-        "product_proposal_id": productProposalId,
-        "product_name": productName,
-        "product_category": productCategory,
-        "product_unit": productUnit,
-        "product_erp_id": productErpId,
-        "erp_id": erpId,
-        "company_product_id": companyProductId,
-        "updated_at": updatedAt?.toIso8601String(),
-        "price": price,
-        "url": url,
-        "description": description,
-        "product_id": productId,
-        "status": status,
-        "update_count": updateCount,
-        "equivalent_id": equivalentId,
-        "proposal_note": proposalNote,
-        "tax_rate": taxRate,
-        "currency_code": currencyCode,
-        "amount": amount,
-      };
+  Map<String, dynamic> toMap() {
+    return {
+      "product_proposal_id": productProposalId,
+      "product_name": productName,
+      "product_category": productCategory,
+      "product_unit": productUnit,
+      "product_erp_id": productErpId,
+      "erp_id": erpId,
+      "company_product_id": companyProductId,
+      "updated_at": updatedAt?.toIso8601String(),
+      "price": price,
+      "url": url,
+      "description": description,
+      "product_id": productId,
+      "status": status,
+      "update_count": updateCount,
+      "equivalent_id": equivalentId,
+      "proposal_note": proposalNote,
+      "tax_rate": taxRate,
+      "currency_code": currencyCode,
+      "amount": amount,
+    };
+  }
 }
